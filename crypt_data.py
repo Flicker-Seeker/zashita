ea17f0693838f91a34babcf8d778536dda870c47
alexey <lesha.pimenov.03@gmail.com>
2024-10-29 20:15:01 +0300

    shitty laba by Simonov

--- crypt_data.py
+++ crypt_data.py
@@ -94,6 +94,8 @@ class CryptEncryptDataBase:
         return False
 
     def create_new_file(self):
+        if not os.path.exists('crypt_data'):
+            os.makedirs('crypt_data')
         if not os.path.exists('/crypt_data/crypt_data.bin') or not os.path.exists('/crypt_data/key.bin'):
             new_user = User(id=1, username='admin',password='1111', is_ban=0, is_admin=1, password_limit=0)
             self.users.append(new_user)
